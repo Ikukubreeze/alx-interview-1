6dc2290b01a664a417121aa367e4ce1dd58d070e
Chinwendu Evans <tisevadon085@gmail.com>
2023-07-14 01:34:06 +0100

    Lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,17 +1,17 @@
-Lockboxes
 #!/usr/bin/python3
 '''LockBoxes Challenge'''
 
+
 def canUnlockAll(boxes):
-'''determines if all the boxes can be opened or not
-Returns:
-True: all boxes can be opened
-False: not all boxes can be opened
-'''
-length = len(boxes)
-keys = set()
-opened_boxes = []
-i = 0
+    '''determines if all the boxes can be opened or not
+    Returns:
+        True: all boxes can be opened
+        False: not all boxes can be opened
+    '''
+    length = len(boxes)
+    keys = set()
+    opened_boxes = []
+    i = 0
 
     while i < length:
         oldi = i
